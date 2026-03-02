8db508e0ad3536fe4099f65f0c881f98ce295a55
lotanna <lotanna006@gmail.com>
2026-03-02 14:44:00 +0100

    update

--- vax gym/src/Header.jsx
+++ vax gym/src/Header.jsx
@@ -1,35 +1,18 @@
-import React from 'react';
-
-
-
 function Header() {
   return (
-    <header style={{ 
-      backgroundColor: '#450693', 
-      padding: '20px 40px',
-      display: 'flex',
-      justifyContent: 'space-between'
-    }}>
-      
-      {/* LEFT: Logo */}
+    <header className="header">
       <div>
-        <img src="/VexGymLogo.png" alt="Vax Gym Logo" style={{ height: '100px' }} />
-
+        <img src="VexGymLogo.png" alt="Vax Gym Logo" className="logo" />
       </div>
-      
-      {/* RIGHT: Menu */}
-      <nav>
-        <a href="#home" style={{ color: '#FFC400', margin: '0 20px' }}>Home</a>
-        <a href="#about" style={{ color: '#FFC400', margin: '0 20px' }}>About</a>
-        <a href="#Services" style={{ color: '#FFC400', margin: '0 20px' }}>Services</a>
-        <a href="#Prices" style={{ color: '#FFC400', margin: '0 20px' }}>Prices</a>
-        <a href="#Testimonial" style={{ color: '#FFC400', margin: '0 20px' }}>Testimonials</a>
-        <a href="#contact" style={{ color: '#FFC400', margin: '0 20px' }}>Contact</a>
+      <nav className="nav">
+        <a href="#home">Home</a>
+        <a href="#about">About</a>
+        <a href="#Services">Services</a>
+        <a href="#Prices">Prices</a>
+        <a href="#Testimonial">Testimonials</a>
+        <a href="#contact">Contact</a>
       </nav>
-      
     </header>
   );
 }
-
 export default Header;
-
