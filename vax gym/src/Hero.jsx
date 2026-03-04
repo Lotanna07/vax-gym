d96330d9097829397510c1736b55b90f61d3c789
lotanna <lotanna006@gmail.com>
2026-03-04 19:39:51 +0100

    update

--- vax gym/src/Hero.jsx
+++ vax gym/src/Hero.jsx
@@ -1,7 +1,7 @@
 function Hero() {
   return (
     <section className="hero">
-      <img src="/Nutra.jpg" alt="Gym" className="hero-image" />
+      <img src="/FREE.jpg" alt="Gym" className="hero-image" />
       <div className="hero-overlay"></div>
       <div className="hero-content">
         <h1>"Get that dream body you've always wanted"</h1>
