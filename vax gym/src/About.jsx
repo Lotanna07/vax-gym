854d569b9c48e6b1a1b58182d274b40302d902a2
lotanna <lotanna006@gmail.com>
2026-03-06 17:50:39 +0100

    update

--- vax gym/src/About.jsx
+++ vax gym/src/About.jsx
@@ -5,7 +5,8 @@ function About() {
           <h1>About Us</h1>
         </div>
         <div className="about-content">
-        <p>At Vax Gym, we're more than just a gym—we're your partner in transformation. Founded by passionate fitness experts right here in Lagos,
+        <p>
+           At Vax Gym, we're more than just a gym—we're your partner in transformation. Founded by passionate fitness experts right here in Lagos,
            we blend cutting-edge equipment, expert-led classes, and personalized training to help you crush your goals, whether you're a beginner building strength or an athlete pushing limits. 
            Join our vibrant community of driven individuals and discover the energy, support, and results that make every sweat session count.
           </p>
---
+++ vax gym/src/Services.jsx
@@ -0,0 +1,14 @@
+function Services(){
+    return(
+        <section className="Services">
+            <div className="Service-title">
+                <h1>Services</h1>
+
+            </div>
+
+        </section>
+
+    )
+}
+
+export default Services ;
